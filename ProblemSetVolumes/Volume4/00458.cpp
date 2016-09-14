9cd0f4c86afb2a3f124e901f6f06e914b4fb745c
TheoKlein <theokleintw@gmail.com>
2016-09-14 20:19:12 +0800

    add 00458.cpp

---
+++ ProblemSetVolumes/Volume4/00458.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string input;
+    while(getline(cin, input)){
+        for(int i = 0 ; i < input.length(); ++i)
+            cout << (char)(input[i] - 7);
+        cout << endl;
+    }
+}
